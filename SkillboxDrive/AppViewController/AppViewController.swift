d8b957399fe8f5d0d876772ac2e3dc38084de291
Ivan <xray86rus@gmail.com>
2024-01-10 20:21:57 +0500

    Создание проекта

---
+++ SkillboxDrive/AppViewController/AppViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AppViewController.swift
+//  SkillboxDrive
+//
+//  Created by Иван Бондаренко on 10/01/24.
+//
+
+import UIKit
+
+class AppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    var image = R.
+}
