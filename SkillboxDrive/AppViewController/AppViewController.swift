4307fe4fea087522625c2418d5f4e569073dc981
Ivan <xray86rus@gmail.com>
2024-01-10 21:45:13 +0500

    Добавил R.swift в проект

--- SkillboxDrive/AppViewController/AppViewController.swift
+++ SkillboxDrive/AppViewController/AppViewController.swift
@@ -5,14 +5,29 @@
 //  Created by Иван Бондаренко on 10/01/24.
 //
 
+import RswiftResources
 import UIKit
 
 class AppViewController: UIViewController {
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView(image: R.image.imageThree())
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+
+        view.addSubview(imageView)
+        setupConstraints()
         
     }
-    var image = R.
+    
+    func setupConstraints() {
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
 }
