5ea64f4b0fd287397bfe4b152985fa049f9ef5cb
Ivan <xray86rus@gmail.com>
2024-01-18 20:32:12 +0500

    починил r.swift

--- SkillboxDrive/Presentation/AppViewController/AppViewController.swift
+++ SkillboxDrive/Presentation/AppViewController/AppViewController.swift
@@ -8,25 +8,10 @@
 import UIKit
 
 class AppViewController: UIViewController {
-    
-    private lazy var imageView: UIImageView = {
-        let image = UIImageView(image: R.image.imageThree())
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-
-        view.addSubview(imageView)
-        setupConstraints()
-        
-    }
-    
-    func setupConstraints() {
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
 }
