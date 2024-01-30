866e839fd7cb012fae96cf2eca54ad33846b496c
Ivan <xray86rus@gmail.com>
2024-01-30 20:33:51 +0500

    bugfix

---
+++ SkillboxDrive/Presentation/AllFilesViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AllFilesViewController.swift
+//  SkillboxDrive
+//
+//  Created by Иван Бондаренко on 18/01/24.
+//
+
+import UIKit
+
+class AllFilesViewController: UIViewController {
+}
---
+++ SkillboxDrive/Presentation/LastFilesViewController.swift
@@ -0,0 +1,11 @@
+//
+//  LastFilesViewController.swift
+//  SkillboxDrive
+//
+//  Created by Иван Бондаренко on 18/01/24.
+//
+
+import UIKit
+
+class LastFilesViewController: UIViewController {
+}
