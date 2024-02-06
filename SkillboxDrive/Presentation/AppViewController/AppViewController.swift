6de3746d095b9b60e322b6dcc08c685875a7a8d3
Ivan <xray86rus@gmail.com>
2024-02-06 19:45:53 +0500

    /.

--- SkillboxDrive/Presentation/AppViewController/AppViewController.swift
+++ SkillboxDrive/Presentation/AppViewController/AppViewController.swift
@@ -37,6 +37,7 @@ class AppViewController: UIViewController {
         publishedFilesVC.tabBarItem.image = R.image.profile()
         allFilesVC.tabBarItem.image = R.image.allFiles()
         rootViewController.viewControllers = [lastFilesVC, publishedFilesVC, allFilesVC]
+        let c = 66
         return rootViewController
     }
     
